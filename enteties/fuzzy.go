fc1c0f31928992c2327f037bca9ac85e72d6065c
VadimRight <vadimright@mail.ru>
2024-05-30 01:50:11 +0300

    Fit: iter through hole tree

--- enteties/fuzzy.go
+++ enteties/fuzzy.go
@@ -2,52 +2,53 @@ package modules
 
 import (
 	"errors"
+	"fmt"
 	"log"
 )
 
-type FuzzyNode struct {
+type NodeFuzzy struct {
 	Value      int
 	Membership float64
-	Left       *FuzzyNode
-	Right      *FuzzyNode
+	Left       *NodeFuzzy
+	Right      *NodeFuzzy
 }
 
 type BSTFuzzy struct {
-	Root *FuzzyNode
+	Root *NodeFuzzy
 }
 
-func (bst *BSTFuzzy) InsertFuzzy(node *FuzzyNode, value int, membership float64) {
+func (bst *BSTFuzzy) InsertFuzzy(node *NodeFuzzy, value int, membership float64) {
 	const opt string = "enteties.fuzzy.InsertFuzzy"
 	if membership > 1.0 {
 		err := errors.New("Membership can't be bigger than 1.0")
 		log.Fatalf("%s: %v", opt, err)
 	}
-	bst.InsertFuzzyNode(bst.Root, value, membership)
+	bst.InsertNodeFuzzy(bst.Root, value, membership)
 }
 
-func (bst *BSTFuzzy) SearchFuzzy(node *FuzzyNode, value int, membership float64) bool {
-	found := bst.SearchFuzzyNode(node, value, membership)
+func (bst *BSTFuzzy) SearchFuzzy(node *NodeFuzzy, value int, membership float64) bool {
+	found := bst.SearchNodeFuzzy(node, value, membership)
 	return found
 }
 
-func (bst *BSTFuzzy) InsertFuzzyNode(node *FuzzyNode, value int, membership float64) *FuzzyNode {
+func (bst *BSTFuzzy) InsertNodeFuzzy(node *NodeFuzzy, value int, membership float64) *NodeFuzzy {
 	if bst.Root == nil {
-		bst.Root = &FuzzyNode{value, membership, nil, nil}
+		bst.Root = &NodeFuzzy{value, membership, nil, nil}
 		return bst.Root
 	}
 	if node == nil {
-		return &FuzzyNode{value, membership, nil, nil}
+		return &NodeFuzzy{value, membership, nil, nil}
 	}
 	if membership <= node.Membership {
-		node.Left = bst.InsertFuzzyNode(node.Left, value, membership)
+		node.Left = bst.InsertNodeFuzzy(node.Left, value, membership)
 	}
 	if membership >= node.Membership {
-		node.Right = bst.InsertFuzzyNode(node.Right, value, membership)
+		node.Right = bst.InsertNodeFuzzy(node.Right, value, membership)
 	}
 	return node
 }
 
-func (bst *BSTFuzzy) SearchFuzzyNode(node *FuzzyNode, value int, membership float64) bool {
+func (bst *BSTFuzzy) SearchNodeFuzzy(node *NodeFuzzy, value int, membership float64) bool {
 	if node.Value == value && node.Membership == membership {
 		return true
 	}
@@ -55,10 +56,39 @@ func (bst *BSTFuzzy) SearchFuzzyNode(node *FuzzyNode, value int, membership floa
 		return false
 	}
 	if membership < node.Membership {
-		return bst.SearchFuzzyNode(node.Left, value, membership)
+		return bst.SearchNodeFuzzy(node.Left, value, membership)
 	}
 	if membership > node.Membership {
-		return bst.SearchFuzzyNode(node.Right, value, membership)
+		return bst.SearchNodeFuzzy(node.Right, value, membership)
 	}
 	return false
+}
+
+func (bst *BSTFuzzy) InOrder(node *NodeFuzzy) {
+	if node == nil {
+		return
+	} else {
+		bst.InOrder(node.Left)
+		fmt.Print(node.Value, node.Membership, " ")
+		bst.InOrder(node.Right)
+	}
+}
+
+func (bst *BSTFuzzy) Levelorder() {
+	if bst.Root == nil {
+		return
+	}
+	nodeList := make([](*NodeFuzzy), 0)
+	nodeList = append(nodeList, bst.Root)
+	for !(len(nodeList) == 0) {
+		current := nodeList[0]
+		fmt.Print(current.Value, " ")
+		if current.Left != nil {
+			nodeList = append(nodeList, current.Left)
+		}
+		if current.Right != nil {
+			nodeList = append(nodeList, current.Right)
+		}
+		nodeList = nodeList[1:]
+	}
 }
