eee3f6e2a1a32046f181b51a76298203c573bcb7
VadimRight <vadimright@mail.ru>
2024-05-30 01:37:25 +0300

    Fit: outer func is ready gitignore is here

--- enteties/fuzzy.go
+++ enteties/fuzzy.go
@@ -1,5 +1,10 @@
 package modules
 
+import (
+	"errors"
+	"log"
+)
+
 type FuzzyNode struct {
 	Value      int
 	Membership float64
@@ -9,6 +14,20 @@ type FuzzyNode struct {
 
 type BSTFuzzy struct {
 	Root *FuzzyNode
+}
+
+func (bst *BSTFuzzy) InsertFuzzy(node *FuzzyNode, value int, membership float64) {
+	const opt string = "enteties.fuzzy.InsertFuzzy"
+	if membership > 1.0 {
+		err := errors.New("Membership can't be bigger than 1.0")
+		log.Fatalf("%s: %v", opt, err)
+	}
+	bst.InsertFuzzyNode(bst.Root, value, membership)
+}
+
+func (bst *BSTFuzzy) SearchFuzzy(node *FuzzyNode, value int, membership float64) bool {
+	found := bst.SearchFuzzyNode(node, value, membership)
+	return found
 }
 
 func (bst *BSTFuzzy) InsertFuzzyNode(node *FuzzyNode, value int, membership float64) *FuzzyNode {
